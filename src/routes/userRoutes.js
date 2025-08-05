33b5a7df74d540be5e126a1317fd65ab410a0047
Gabriel Delfes <106628024+Gabrielmd3@users.noreply.github.com>
2025-08-05 13:53:47 -0300

    esclarecimento de rotas

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,10 +5,13 @@ import { authMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 // Registro e login
+//POST http://localhost:3000/api/users/register
 router.post('/register', UserController.register);
+//POST http://localhost:3000/api/users/login
 router.post('/login', UserController.login);
 
 // Rota protegida para pegar usuário atual a partir do token do usuario
+//GET http://localhost:3000/api/users/current 
 router.get('/current', authMiddleware.verifyToken, UserController.getCurrent);
 
 export default router;
